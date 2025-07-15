b60650d9a7ebbfeec55d338cdea9462db3606fd4
Nikhil Singh <nik.singh710@gmail.com>
2025-07-15 13:34:59 +0530

    refactor: Add review comments

--- dev/flake.nix
+++ dev/flake.nix
@@ -2,12 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    python-flake.url = "path:../."; # TODO: use a proper flake once upstreamed
     just-flake.url = "github:juspay/just-flake";
 
     git-hooks.url = "github:cachix/git-hooks.nix";
-    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    git-hooks.flake = false;
   };
 
   outputs = inputs@{ nixpkgs, flake-parts, ... }:
@@ -15,27 +13,19 @@
       systems = nixpkgs.lib.systems.flakeExposed;
       debug = true;
       imports = [
-        inputs.git-hooks.flakeModule
+        (inputs.git-hooks + /flake-module.nix)
         inputs.just-flake.flakeModule
-        inputs.treefmt-nix.flakeModule
       ];
 
       perSystem = { pkgs, config, ... }: {
         pre-commit = {
           check.enable = true;
           settings.hooks = {
-            treefmt.enable = true;
+            nixpkgs-fmt.enable = true;
             convco.enable = true;
           };
         };
-        treefmt.config = {
-          projectRoot = inputs.python-flake;
-          projectRootFile = "flake.nix";
-          flakeCheck = false; # pre-commit-hooks.nix checks this
-          programs.nixpkgs-fmt.enable = true;
-        };
         just-flake.features = {
-          treefmt.enable = true;
           convco.enable = true;
         };
         devShells.default = pkgs.mkShell {
@@ -43,7 +33,6 @@
           # cf. https://community.flake.parts/haskell-flake#composing-devshells
           inputsFrom = [
             config.just-flake.outputs.devShell
-            config.treefmt.build.devShell
             config.pre-commit.devShell
           ];
           packages = [
--- flake.nix
+++ flake.nix
@@ -2,8 +2,27 @@
   description = "A `flake-parts` module for Python development";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    uv2nix.url = "github:pyproject-nix/uv2nix";
+    uv2nix.inputs.pyproject-nix.follows = "pyproject-nix";
+    uv2nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    pyproject-build-systems.url = "github:pyproject-nix/build-system-pkgs";
+    pyproject-build-systems.inputs.pyproject-nix.follows = "pyproject-nix";
+    pyproject-build-systems.inputs.uv2nix.follows = "uv2nix";
+    pyproject-build-systems.inputs.nixpkgs.follows = "nixpkgs";
+
+    uv2nix_hammer_overrides.url = "github:TyberiusPrime/uv2nix_hammer_overrides";
+    uv2nix_hammer_overrides.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ ... }: {
+  outputs = { ... }: {
+    flakeModules = {
+      default = ./nix/modules/flake-module.nix;
+      nixpkgs = ./nix/modules/nixpkgs.nix;
+    };
     om.ci.default =
       let
         overrideInputs = {
