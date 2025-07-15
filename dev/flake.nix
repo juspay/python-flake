70c0f7e5fafbde15ef6605c9c93c40e42f0f440a
Nikhil Singh <nik.singh710@gmail.com>
2025-07-15 20:16:58 +0530

    refactor: review comments complied

--- dev/flake.nix
+++ dev/flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    just-flake.url = "github:juspay/just-flake";
 
     git-hooks.url = "github:cachix/git-hooks.nix";
     git-hooks.flake = false;
@@ -14,29 +13,18 @@
       debug = true;
       imports = [
         (inputs.git-hooks + /flake-module.nix)
-        inputs.just-flake.flakeModule
       ];
 
       perSystem = { pkgs, config, ... }: {
         pre-commit = {
           check.enable = true;
-          settings.hooks = {
-            nixpkgs-fmt.enable = true;
-            convco.enable = true;
-          };
-        };
-        just-flake.features = {
-          convco.enable = true;
+          settings.hooks.nixpkgs-fmt.enable = true;
         };
         devShells.default = pkgs.mkShell {
           name = "python-flake";
           # cf. https://community.flake.parts/haskell-flake#composing-devshells
           inputsFrom = [
-            config.just-flake.outputs.devShell
             config.pre-commit.devShell
-          ];
-          packages = [
-            config.pre-commit.settings.tools.convco
           ];
         };
       };
